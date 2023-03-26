ec64de0269ded18b57b14e55c2a960cf03a6d5e7
mehmet ali kuyucu <mkuyucu443@gmail.com>
2023-03-26 22:01:46 +0300

    feat:added select option logic

--- script.js
+++ script.js
@@ -102,5 +102,11 @@ async function fetchData() {
 
     // }
 
+
+}
+document.getElementById('demo1').addEventListener("change",handleChange)
+function handleChange(){
+console.log(document.getElementById('demo1').value);
+document.getElementById('demo1').innerHTML="<option name='12312'>sdfsdf</option>"
 }
 fetchData();
